02051fce441bfb05260f6460f71f2e4c4c4cc99a
preferz <106276822+preferz@users.noreply.github.com>
2022-06-10 21:27:26 +0100

    Update main.go

--- ProxyScraper/main.go
+++ ProxyScraper/main.go
@@ -1,41 +1,121 @@
 package main
 
-import( 
+import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	"strings"
+	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
+func GetSock4() {
 
-	fmt.Println("Starting...")
-
-	r, err := http.Get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=10000&country=all&anonymity=all&ssl=no")
+	r, err := http.Get("https://api.openproxylist.xyz/socks4.txt")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 	prox, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	fmt.Println("Writing To File...")
 
 	proxies := strings.TrimSuffix(string(prox), "\n")
-	file, err := os.OpenFile("Proxy/proxies.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("Proxy/socks4.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println("File Not found!")
-		os.Exit(0)
+		panic(err)
 	}
 	defer file.Close()
 
 	_, err = fmt.Fprint(file, proxies)
 	if err != nil {
-		file.Close()
-		fmt.Println(err)
+		panic(err)
 	}
-	fmt.Println("Scraped! | Saved to proxies.txt")
+	fmt.Println("Scraped! | Saved to socks4.txt")
+
+}
+
+func GetSock5() {
+
+	r, err := http.Get("https://api.openproxylist.xyz/socks5.txt")
+	if err != nil {
+		panic(err)
+	}
+	prox, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Writing To File...")
+
+	proxies := strings.TrimSuffix(string(prox), "\n")
+	file, err := os.OpenFile("Proxy/socks5.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprint(file, proxies)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Scraped! | Saved to socks5.txt")
+
+}
+
+func GetHttp() {
+
+	r, err := http.Get("https://api.openproxylist.xyz/http.txt")
+	if err != nil {
+		panic(err)
+	}
+	prox, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Writing To File...")
+
+	proxies := strings.TrimSuffix(string(prox), "\n")
+	file, err := os.OpenFile("Proxy/http.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprint(file, proxies)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Scraped! | Saved to http.txt")
+
+}
+
+func main() {
+
+	var banner string = `
+	[1] Socks4
+	[2] Socks5
+	[3] HTTP
+	`
+
+	fmt.Println(banner)
+
+	var choice string
+	fmt.Printf("Enter Choice: ")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case "1":
+		GetSock4()
+	case "2":
+		GetSock5()
+	case "3":
+		GetHttp()
+	default:
+		fmt.Println("Invalid Choice!")
+
+	}
 
 }
